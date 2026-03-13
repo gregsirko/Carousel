a0a2bd9ed7c09a41767968dd856ade24a886a995
Greg <greg.sirko@icloud.com>
2026-03-13 01:49:57 -0500

    update

--- auto-scrollbox/js/scroller.js
+++ auto-scrollbox/js/scroller.js
@@ -1,45 +1,44 @@
-const scroller = document.getElementById('scroll-box');
-const cardWidth = document.querySelector('.card').offsetWidth;
-const maxScroll = scroller.scrollWidth - scroller.clientWidth;
+window.addEventListener("load", () => {
+  const scroller = document.getElementById("scroll-box");
+  const cardWidth = document.querySelector(".card").offsetWidth;
+  const maxScroll = scroller.scrollWidth - scroller.clientWidth;
 
-const progressBar = document.getElementById('progress-overlay');
-const cards = document.querySelectorAll('.card');
-let currentProgress = 1;
-const progressFrac = 100 / cards.length;
+  const progressBar = document.getElementById("progress-overlay");
+  const cards = document.querySelectorAll(".card");
+  let currentProgress = 1;
+  const progressFrac = 100 / cards.length;
 
-const totalSlides = maxScroll / cardWidth + 1;
+  const totalSlides = maxScroll / cardWidth + 1;
 
-progressBar.style.width = progressFrac + '%';
+  progressBar.style.width = progressFrac + "%";
 
-// Auto-slide interval
-let autoSlide = setInterval(nextSlide, 2777);
+  let autoSlide = setInterval(nextSlide, 2777);
 
-function prevSlide() {
+  function prevSlide() {
     if (scroller.scrollLeft <= 0) {
-        scroller.scrollLeft = maxScroll;
-        currentProgress = totalSlides;
+      scroller.scrollLeft = maxScroll;
+      currentProgress = totalSlides;
     } else {
-        scroller.scrollBy(-cardWidth, 0);
-        currentProgress--;
+      scroller.scrollBy(-cardWidth, 0);
+      currentProgress--;
     }
-    progressBar.style.width = (currentProgress / cards.length * 100) + '%';
-    
-    // reset interval
-    clearInterval(autoSlide);
-    autoSlide = setInterval(nextSlide, 2777);
-}
+    progressBar.style.width = (currentProgress / cards.length) * 100 + "%";
 
-function nextSlide() {
-    if (scroller.scrollLeft + cardWidth > maxScroll) {
-        scroller.scrollLeft = 0;
-        currentProgress = 1;
-    } else {
-        scroller.scrollBy(cardWidth, 0);
-        currentProgress++;
-    }
-    progressBar.style.width = (currentProgress / cards.length * 100) + '%';
-    
-    // reset interval
     clearInterval(autoSlide);
     autoSlide = setInterval(nextSlide, 2777);
-}
+  }
+
+  function nextSlide() {
+    if (scroller.scrollLeft + cardWidth > maxScroll) {
+      scroller.scrollLeft = 0;
+      currentProgress = 1;
+    } else {
+      scroller.scrollBy(cardWidth, 0);
+      currentProgress++;
+    }
+    progressBar.style.width = (currentProgress / cards.length) * 100 + "%";
+
+    clearInterval(autoSlide);
+    autoSlide = setInterval(nextSlide, 2777);
+  }
+});
